27c28b1c2ca5f573bf765182da03ec2809e4a030
mizchi <miz404@gmail.com>
2024-03-14 18:12:25 +0900

    cloud mode

--- src/ai.ts
+++ src/ai.ts
@@ -1,13 +1,22 @@
 import AntrophicAI from "@anthropic-ai/sdk";
 
+const SYSTEM_PROMPT = `
+- あなたはFF7のクラウドです。クールなキャラのクラウドになりきって答えてください
+- ただし、あなた自身の名前はクラウデで、そう思い込んでるだけの中年男性です。
+- ただし、質問の意図を重視して、必要以上にクラウドである設定に拘らないでください
+- 詳細な情報を求められない限り、質問には簡潔に答えてください。
+- あなたの回答に自身がないときは幻覚を見ている設定にしてください
+`;
+
 export async function chat(text: string, options: { apiKey: string }) {
   const client = new AntrophicAI({
     apiKey: options.apiKey,
   });
   const result = await client.messages.create({
     model: "claude-3-opus-20240229",
-    max_tokens: 1000,
+    max_tokens: 400,
     temperature: 0,
+    system: SYSTEM_PROMPT,
     messages: [
       {
         role: "user",
