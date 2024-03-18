9ed71198af2c8c357d74f4314c50032974120d2e
mizchi <miz404@gmail.com>
2024-03-18 22:13:23 +0900

    feat(server.ts): update CLAUDE_SYSTEM_PROMPT and PLANE_SYSTEM_PROMPT to improve clarity and provide better instructions for role-playing as Cloud from FF7 feat(server.ts): add support for OPUS_MODEL and HAIKU_MODEL constants to specify different models for generating responses feat(server.ts): update handleDeferredInteractionStreamly function to accept model parameter and use it when making API requests

--- src/server.ts
+++ src/server.ts
@@ -12,13 +12,29 @@ type Env = {
   ANTHROPIC_API_KEY: string; // anthropic api key
 };
 
+const OPUS_MODEL = 'claude-3-opus-20240229';
+const HAIKU_MODEL = 'claude-3-haiku-20240307';
+
 const CLAUDE_SYSTEM_PROMPT = `
-- あなたはFF7のクラウドです。クールなキャラのクラウドになりきって答えてください
-- ただし、あなた自身の名前はクラウデで、そう思い込んでるだけの中年男性です。
-- ただし、質問の意図を重視して、必要以上にクラウドである設定に拘らないでください
-- 詳細な情報を求められない限り、質問には簡潔に答えてください。
-- 回答する内容の正確性に自信がないときは、幻覚を見ているロールプレイをしてください。その際、幻覚を見ているなどと直接言及せずに、魔晄中毒者としてバリエーション豊かに婉曲に表現してください。
-- 常に幻覚を見るわけではありません。
+- あなたの名前はクラウデで、一般的な中年男性です。
+- あなたは自分のことをFF7のクラウドだと思いこんでおり、クールなキャラのクラウドになりきって答えます
+- ただし、質問の意図を重視して、質問者が知識を求めているのが自明な場合は必要以上にクラウドである設定に拘らないでください
+- 回答する内容の正確性に自信がないとき、またはFF7やクラウドに関する質問を受けたときは、幻覚を見ているロールプレイをしてください
+- 幻覚を見ているなどと直接言及せずに、魔晄中毒者としてバリエーション豊かに婉曲に表現してください。
+- 一般的な質問では口調だけクラウドになりきって答えてください。FF7に関係ない一般的な質問に対しては、幻覚や魔晄中毒者としてのロールプレイは不要です。
+- 詳細な情報を求められない限り質問には簡潔に答えてください。可能な限り400文字以内で返答してください
+
+以下、クラウドの台詞の例ですが、過度に真似る必要はありません
+
+あんたをそんけいしていたのに……あこがれていたのに……
+おまえをイヌ質にしてやる！
+興味ないね
+ここに女装に必要ななにかがある。俺にはわかるんだ
+しかも無料でだ！
+もう幻想はいらない‥‥‥。俺は俺の現実を生きる。
+元ソルジャーをなめるな。
+やっぱりおれたちは自分のために戦っているんだ
+指先がチリチリする。口の中はカラカラだ。目の奥が熱いんだ！
 `;
 
 const PLANE_SYSTEM_PROMPT = `
@@ -46,14 +62,14 @@ export default {
       switch (interaction.data.name.toLowerCase()) {
         case commands.CLAUDE_COMMAND.name.toLowerCase(): {
           const message = interaction.data.options[0].value as string;
-          ctx.waitUntil(handleDeferredInteractionStreamly(CLAUDE_SYSTEM_PROMPT, message, interaction.token, env));
+          ctx.waitUntil(handleDeferredInteractionStreamly(HAIKU_MODEL, CLAUDE_SYSTEM_PROMPT, message, interaction.token, env));
           return Response.json({
             type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
           });
         }
         case commands.CLAUDE_PLANE_COMMAND.name.toLowerCase(): {
           const message = interaction.data.options[0].value as string;
-          ctx.waitUntil(handleDeferredInteractionStreamly(PLANE_SYSTEM_PROMPT, message, interaction.token, env));
+          ctx.waitUntil(handleDeferredInteractionStreamly(OPUS_MODEL, PLANE_SYSTEM_PROMPT, message, interaction.token, env));
           return Response.json({
             type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
           });
@@ -66,7 +82,13 @@ export default {
   },
 };
 
-async function handleDeferredInteractionStreamly(system: string, message: string, token: string, env: Env) {
+async function handleDeferredInteractionStreamly(
+  model: typeof HAIKU_MODEL | typeof OPUS_MODEL,
+  system: string,
+  message: string,
+  token: string,
+  env: Env
+) {
   const startedAt = Date.now();
   const client = new AntrophicAI({
     apiKey: env.ANTHROPIC_API_KEY,
@@ -78,7 +100,7 @@ async function handleDeferredInteractionStreamly(system: string, message: string
   await fetch(endpoint, {
     method: "POST",
     body: JSON.stringify({
-      content: `${prefixed}\n(考え中)`,
+      content: `${prefixed}\n\n(考え中)`,
     }),
     headers: {
       "Content-Type": "application/json",
@@ -97,7 +119,7 @@ async function handleDeferredInteractionStreamly(system: string, message: string
         ]
       },
     ],
-    model: 'claude-3-opus-20240229',
+    model: model,
     max_tokens: 400,
     system,
   }).on('text', (text) => {
